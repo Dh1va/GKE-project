e79965a746595e1ad8feca241624b647366cc35a
Dh1va <dhivakar.s3434@gmail.com>
2025-10-13 14:44:54 +0530

    banner update

--- src/components/hero3/hero3.js
+++ src/components/hero3/hero3.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import { Fade } from "react-awesome-reveal";
-import hImg from '../../images/shape/hero-glassisom2.png'
+// import hImg from '../../images/shape/hero-glassisom2.png'
 
 const Hero3 = (props) => {
 
@@ -18,25 +18,25 @@ const Hero3 = (props) => {
                             <div className="xb-hero">
                                 <Fade direction='up' triggerOnce={'false'} duration={800} delay={6}>
                                     <div>
-                                        {/* <h1 className="xb-item--title wow fadeInUp" data-wow-delay="0ms" data-wow-duration="600ms">Powering Smart Buildings with MEP Expertise</h1> */}
+                                        <h1 className="xb-item--title wow fadeInUp"style={{color:"#3450A1"}}  data-wow-delay="0ms" data-wow-duration="600ms">Powering Smart Buildings with MEP Expertise</h1>
                                     </div>
                                 </Fade>
                                 <Fade direction='up' triggerOnce={'false'} duration={1500} delay={9}>
                                     <div>
-                                        {/* <p className="xb-item--content wow fadeInUp" data-wow-delay="150ms" data-wow-duration="600ms">We deliver efficient, safe, and sustainable MEP solutions covering HVAC, electrical, plumbing, and more to optimize building performance at every stage.</p> */}
+                                        <p className="xb-item--content wow fadeInUp" style={{color:"#000"}} data-wow-delay="150ms" data-wow-duration="600ms">We deliver efficient, safe, and sustainable MEP solutions covering HVAC, electrical, plumbing, and more to optimize building performance at every stage.</p>
                                     </div>
                                 </Fade>
                                 <Fade direction='up' triggerOnce={'false'} duration={1800} delay={9}>
                                     <div>
                                         <div className="xb-btn mt-50 wow fadeInUp" data-wow-delay="300ms" data-wow-duration="600ms">
-                                            {/* <Link onClick={ClickHandler} to="/contact" className="thm-btn thm-btn--fill_icon thm-btn--data">
-                                                <div className="xb-item--hidden"><span className="xb-item--hidden-text">Start Your Project Discussion</span></div>
+                                            <Link onClick={ClickHandler} to="/contact" className="thm-btn thm-btn--fill_icon thm-btn--data">
+                                                <div className="xb-item--hidden"><span className="xb-item--hidden-text">Talk To Experts</span></div>
                                                 <div className="xb-item--holder">
-                                                    <span className="xb-item--text xb-item--text1">Start Your Project Discussion</span>
+                                                    <span className="xb-item--text xb-item--text1">Talk To Experts</span>
                                                     <div className="xb-item--icon"><i className="fal fa-plus"></i></div>
-                                                    <span className="xb-item--text xb-item--text2">Start Your Project Discussion</span>
+                                                    <span className="xb-item--text xb-item--text2">Talk To Experts</span>
                                                 </div>
-                                            </Link> */}
+                                            </Link>
                                         </div>
                                     </div>
                                 </Fade>
